8e2df390d7d57681476abf32e9d19d0acf5e3d8a
singhbhupinder651 <71844883+singhbhupinder651@users.noreply.github.com>
2022-10-31 12:40:55 +0530

    updated

---
+++ ImplementInterface/ImplementInterface/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ImplementInterface;
+
+Console.WriteLine("Hello, World!");
+
+add obj = new add();
+obj.addition(4,5);
---
+++ ImplementInterface/ImplementInterface/add.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImplementInterface
+{
+    public class add : calc
+    {
+        public void addition(int a, int b)
+        {
+            Console.WriteLine("Addition of two numbers");
+            int result = a + b;
+            Console.WriteLine(result);
+        }
+    }
+}
